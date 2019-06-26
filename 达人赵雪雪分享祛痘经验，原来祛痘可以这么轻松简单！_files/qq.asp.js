1b1c585b28f7c4c71eb96aea0e0b56f72fd6f15c
Sailboats <caoligai520402@gmail.com>
2019-06-27 00:32:58 +0800

    Add files via upload

---
+++ 达人赵雪雪分享祛痘经验，原来祛痘可以这么轻松简单！_files/qq.asp.js
@@ -0,0 +1,15 @@
+
+var notname=document.getElementsByName('notname');
+      for(i=0;i<notname.length;i++){
+      notname[i].innerHTML='1';
+      }
+var notqq=document.getElementsByName('notqq');
+      for(i=0;i<notqq.length;i++){
+      notqq[i].innerHTML='·ëÑÅÇç';
+      }
+var notweixin=document.getElementsByName('notweixin');
+      for(i=0;i<notweixin.length;i++){
+      notweixin[i].innerHTML='bg5593';
+      }
+//document.getElementByName("notqq").innerHTML='·ëÑÅÇç';
+//document.getElementByName("notweixin").innerHTML='bg5593';
